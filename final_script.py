46a9637a01fd85e4346aa8d64c2deb65984ba419
DomLang <95230300+DomLang@users.noreply.github.com>
2022-06-01 14:27:27 +0200

    Add files via upload

--- final_script.py
+++ final_script.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 '''
 Code created by Dominic Langhammer, Bayerisches Geoinstitut, Bayreuth, Germany \n
-Version: 17th Jan. 2022
+Version: 01/06/2022
 '''
 
 st.title('Viscosity Calculator')
